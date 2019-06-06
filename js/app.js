0bbb7fa095f2a791ba49c984c7fa5fe10af41bda
Alex <nikitin2009@gmail.com>
2019-06-06 15:25:14 +0300

    Init

---
+++ js/app.js
@@ -0,0 +1,16 @@
+let myLibrary = [];
+
+function Book({title, author, pages, read}) {
+  this.title = title
+  this.author = author
+  this.pages = pages
+  this.read = read
+  this.info = function() {
+    let readText = this.read ? "read" : "not read yet";
+    return `${this.title} by ${this.author}, ${this.pages} pages, ${readText}`;
+  }
+}
+
+function addBookToLibrary() {
+  myLibrary.push(new Book(book));
+}
