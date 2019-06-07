e23aaa1555eaa0bc638d5841de7e2a2a6053e184
Alex <nikitin2009@gmail.com>
2019-06-07 12:50:46 +0300

    Add New Book form

--- js/app.js
+++ js/app.js
@@ -26,13 +26,28 @@ myLibrary.push(new Book({
 }));
 
 // Helper function
-function addBookToLibrary() {
-  // TO-DO
+function addBookToLibrary(book) {
   myLibrary.push(new Book(book));
 }
 
+function handleNewBookSubmit(e) {
+  e.preventDefault();
+
+  let formElements = e.target.elements;
+  let bookParams = {
+    title: formElements["title"].value,
+    author: formElements["author"].value,
+    pages: Number(formElements["pages"].value),
+    read: formElements["read"].checked
+  };
+
+  addBookToLibrary(bookParams);
+  render();
+}
+
 function showNewBookForm() {
-  console.log("show form");
+  const newBookForm = document.getElementById('newBookForm');
+  newBookForm.classList.toggle('d-none');
 }
 
 // Render functions
@@ -40,7 +55,7 @@ function renderHeader() {
   const pageHeading = "<h1 class='text-center'>Books shelf</h1>";
   
   const newBookButton = document.createElement("button");
-  newBookButton.className = "btn btn-primary btn-add-book mt-4";
+  newBookButton.className = "btn btn-outline-primary btn-add-book mt-4";
   newBookButton.innerText = "New Book";
   newBookButton.onclick = showNewBookForm;
 
@@ -50,6 +65,32 @@ function renderHeader() {
   header.appendChild(newBookButton);
 
   return header;
+}
+
+function renderNewBookForm() {
+  const form = document.createElement('form');
+  form.className = "form-inline mb-4 d-none";
+  form.id = "newBookForm";
+  form.onsubmit = handleNewBookSubmit;
+  form.innerHTML = `
+    <label class="sr-only" for="bookTitle">Title</label>
+    <input name="title" type="text" class="form-control mr-sm-3" id="bookTitle" placeholder="Title" required>
+
+    <label class="sr-only" for="bookAuthor">Author</label>
+    <input name="author" type="text" class="form-control mr-sm-3" id="bookAuthor" placeholder="Author">
+
+    <label class="sr-only" for="bookPages">Pages</label>
+    <input name="pages" type="number" class="form-control mr-sm-3" id="bookPages" placeholder="Pages">
+
+    <div class="custom-control custom-checkbox my-1 mr-sm-5">
+      <input name="read" type="checkbox" class="custom-control-input" id="bookRead">
+      <label class="custom-control-label" for="bookRead">Read</label>
+    </div>
+
+    <button type="submit" class="btn btn-primary">Add Book</button>
+  `;
+
+  return form;
 }
 
 function renderTableRow(book, index) {
@@ -95,11 +136,15 @@ function renderTable(books) {
 
 function render() {
   const app = document.getElementById("app");
+  app.innerHTML = "";
   
   const header = renderHeader();
+
+  const newBookForm = renderNewBookForm();
 
   const booksTable = renderTable(myLibrary);
 
   app.appendChild(header);
+  app.appendChild(newBookForm);
   app.appendChild(booksTable);
 }
