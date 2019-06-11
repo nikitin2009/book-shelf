622dc6f6788ab6201e6c5dac8590b11dfcd21362
Alex <nikitin2009@gmail.com>
2019-06-11 13:10:51 +0300

    Refactor Book class to use ES6 class syntax

--- js/app.js
+++ js/app.js
@@ -1,18 +1,20 @@
 let myLibrary = [];
 
-function Book({title, author, pages, read}) {
-  this.title = title
-  this.author = author
-  this.pages = pages
-  this.read = read
-}
+class Book {
+  constructor({title, author, pages, read}) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
 
-Book.prototype.getReadText = function() {
-  return this.read ? "read" : "not read yet";
-}
+  getReadText() {
+    return this.read ? "read" : "not read yet";
+  }
 
-Book.prototype.toggleRead = function() {
-  this.read = !this.read;
+  toggleRead() {
+    this.read = !this.read;
+  }
 }
 
 // Add a few sample books
